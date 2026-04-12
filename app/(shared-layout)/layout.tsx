3f315fee961a149ffbd56f076160a28c20093ad8
shanmengqi <shanmengqi@gmail.com>
2026-04-13 01:30:32 +0800

    feat: initial commit

---
+++ app/(shared-layout)/layout.tsx
@@ -0,0 +1,11 @@
+// import Navbar from '@/components/Web/navbar'
+import React, { ReactNode } from 'react'
+
+export default function SharedLayout({ children }: { children: ReactNode }) {
+  return (
+    <>
+      {/* <Navbar /> */}
+      {children}
+    </>
+  )
+}
--- app/layout.tsx
+++ app/layout.tsx
@@ -27,7 +27,11 @@ export default function RootLayout({
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col">
+        <main className="max-w-7xl mx-auto w-full px-4 md:px-6 lg:px-8">
+          {children}
+        </main>
+      </body>
     </html>
   );
 }
